482e36fd716b6bb11eb12f90ddfc0b3e73193ce0
marsxnow <raxelportiz@gmail.com>
2024-05-08 23:13:45 -0700

    SideBar
    Alv se mira bien perra mi sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Footer from "./components/Footer";
 import Intro from "./components/Intro";
 import Timeline from "./components/Timeline";
 import Portfolio from "./components/Portfolio";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
       <Intro />
-      <Portfolio />
+      {/* <Portfolio /> */}
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import {
+  AiOutlineHome,
+  AiOutlineMenu,
+  AiOutlineProject,
+  AiOutlineMail,
+} from "react-icons/ai";
+import { BsPerson } from "react-icons/bs";
+
+const sidebar = () => {
+  const [sidebar, setSidebar] = useState(false);
+  const handleSidebar = () => {
+    setSidebar(!sidebar);
+  };
+  return (
+    <div className="sidebar">
+      <AiOutlineMenu
+        onClick={handleSidebar}
+        className="absolute top-4 right-4 z-[99] md:hidden "
+      />
+      {sidebar ? (
+        <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-grey-40 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Home</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-grey-40 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineProject size={20} />
+            <span className="pl-4">Projects</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-grey-40 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <BsPerson size={20} />
+            <span className="pl-4">Resume</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-grey-40 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineMail size={20} />
+            <span className="pl-4">Contact</span>
+          </a>
+        </div>
+      ) : (
+        ""
+      )}
+      <div className="md:block hidden fixed top-[25%] z-10">
+        <div className="flex flex-col">
+          <a
+            href="#main"
+            className="rounded-full shadow-lg bg-grey-100 shadow-grey-400 m-2 p-4 cursor-pointer hover-scale-110 ease-in duration-300"
+          >
+            <AiOutlineHome size={20} />
+          </a>
+          <a
+            href="#main"
+            className="rounded-full shadow-lg bg-grey-100 shadow-grey-400 m-2 p-4 cursor-pointer hover-scale-110 ease-in duration-300"
+          >
+            <AiOutlineProject size={20} />
+          </a>{" "}
+          <a
+            href="#main"
+            className="rounded-full shadow-lg bg-grey-100 shadow-grey-400 m-2 p-4 cursor-pointer hover-scale-110 ease-in duration-300"
+          >
+            <BsPerson size={20} />
+          </a>{" "}
+          <a
+            href="#main"
+            className="rounded-full shadow-lg bg-grey-100 shadow-grey-400 m-2 p-4 cursor-pointer hover-scale-110 ease-in duration-300"
+          >
+            <AiOutlineMail size={20} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default sidebar;
