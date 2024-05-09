0d4381b3043e45443b2302c3e3a4df10a01ceeed
marsxnow <raxelportiz@gmail.com>
2024-05-09 00:37:48 -0700

    Intro page
    Somewhat finished intro page. go back and clean css and get rid of any
    unwanted divs

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,17 +1,46 @@
 import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import { FaTwitter, FaLinkedinIn } from "react-icons/fa";
 
 function Intro() {
   return (
-    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
-        Hey, Im Rax !
-      </h1>
-      <p className="textbase md:text-xl mb-3 font medium">
-        I am working on being a software engineer and web developer.
-      </p>
-      <p className="text-sm max-w-xl mb-6 font-bold">
-        This is my minimalistic website
-      </p>
+    <div
+      id="intro"
+      className="flex items-center justify-center flex-col text-center pb-6"
+    >
+      <img
+        className="w-full h-screen object-cover object-left"
+        src="https://media.discordapp.net/attachments/1179276305729073232/1217525991224639649/20240313_103302.jpg?ex=663db1b0&is=663c6030&hm=fa14b142e79efdc5418f3f1bcf1781d0a8cec8157af2cd94f8b8cc9906c99359&format=webp&width=854&height=1138&"
+      />
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+        <div className="max-w-[700] m-auto h-full flex flex-col justify-center items-center">
+          <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
+            Hey, Im Rax !
+          </h1>
+          <h2 className="textbase md:text-xl sm:text-3xl mb-3 font-medium flex">
+            <TypeAnimation
+              sequence={[
+                "I am a student",
+                1000,
+                "I am an aspiring web developer",
+                1000,
+                "I am an aspiring software engineer",
+                1000,
+                "I am Mexican",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: "2em", display: "inline-block" }}
+              repeat={Infinity}
+            />
+          </h2>
+          <div className="flex justify-between max-w-[100px] w-full">
+            <FaTwitter className="cursor-pointer" size={20} />
+            <FaLinkedinIn className="cursor-pointer" size={20} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,28 +21,28 @@ const sidebar = () => {
       {sidebar ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
           <a
-            href="#main"
+            href="#intro"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-grey-40 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineHome size={20} />
             <span className="pl-4">Home</span>
           </a>
           <a
-            href="#main"
+            href="#project"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-grey-40 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineProject size={20} />
             <span className="pl-4">Projects</span>
           </a>
           <a
-            href="#main"
+            href="#resume"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-grey-40 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <BsPerson size={20} />
             <span className="pl-4">Resume</span>
           </a>
           <a
-            href="#main"
+            href="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-grey-40 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineMail size={20} />
@@ -55,25 +55,25 @@ const sidebar = () => {
       <div className="md:block hidden fixed top-[25%] z-10">
         <div className="flex flex-col">
           <a
-            href="#main"
+            href="#intro"
             className="rounded-full shadow-lg bg-grey-100 shadow-grey-400 m-2 p-4 cursor-pointer hover-scale-110 ease-in duration-300"
           >
             <AiOutlineHome size={20} />
           </a>
           <a
-            href="#main"
+            href="#project"
             className="rounded-full shadow-lg bg-grey-100 shadow-grey-400 m-2 p-4 cursor-pointer hover-scale-110 ease-in duration-300"
           >
             <AiOutlineProject size={20} />
           </a>{" "}
           <a
-            href="#main"
+            href="#resume"
             className="rounded-full shadow-lg bg-grey-100 shadow-grey-400 m-2 p-4 cursor-pointer hover-scale-110 ease-in duration-300"
           >
             <BsPerson size={20} />
           </a>{" "}
           <a
-            href="#main"
+            href="#contact"
             className="rounded-full shadow-lg bg-grey-100 shadow-grey-400 m-2 p-4 cursor-pointer hover-scale-110 ease-in duration-300"
           >
             <AiOutlineMail size={20} />
